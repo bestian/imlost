b61e7759b13e335a6a90f778ce4078db919941a3
Bestian Tang <bestian@gmail.com>
2020-03-18 10:43:30 +0800

    v1 build

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueGtag from 'vue-gtag'
+
+Vue.use(VueGtag, {
+  config: { id: 'UA-26178243-15' }
+})
+
 
 Vue.config.productionTip = false
 
